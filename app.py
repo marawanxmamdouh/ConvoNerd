5b20a3a12c612e9b68edfc519230aa57bc066943
Marawan Mamdouh <marawanxmamdouh@gmail.com>
2023-08-30 12:01:27 +0300

    [Refactor] PDF upload handling and add prompt template.

--- app.py
+++ app.py
@@ -1,13 +1,14 @@
+import os
+
 import streamlit as st
 import torch
-from PyPDF2 import PdfReader
 from dotenv import load_dotenv
-from langchain import HuggingFacePipeline
+from langchain import HuggingFacePipeline, PromptTemplate, FAISS
 from langchain.chains import ConversationalRetrievalChain
+from langchain.document_loaders import PyPDFDirectoryLoader
 from langchain.embeddings import HuggingFaceBgeEmbeddings
 from langchain.memory import ConversationBufferMemory
-from langchain.text_splitter import CharacterTextSplitter
-from langchain.vectorstores import FAISS
+from langchain.text_splitter import RecursiveCharacterTextSplitter
 from transformers import AutoModelForSeq2SeqLM, pipeline, TextStreamer
 from transformers import AutoTokenizer
 
@@ -16,24 +17,45 @@ from html_templates import css, bot_template, user_template
 DEVICE = "cuda:0" if torch.cuda.is_available() else "cpu"
 print(DEVICE)
 
+# %%
+custom_template = """Given the following conversation and a follow up question, rephrase the follow up question to be 
+a standalone question. At the end of standalone question add this 'Answer the question in German language.' If you do 
+not know the answer reply with 'I am sorry'. Chat History: {chat_history} Follow Up Input: {question} Standalone 
+question:"""
 
+# %%
+
+prompt = PromptTemplate.from_template(template=custom_template)  # , input_variables=["context", "question"]
+
+
+# %%
 def get_pdf_text(pdf_docs):
-    text = ""
-    for pdf in pdf_docs:
-        pdf_reader = PdfReader(pdf)
-        for page in pdf_reader.pages:
-            text += page.extract_text()
-    return text
+    # delete old files
+    if os.path.isdir('uploaded_files'):
+        for f in os.listdir('uploaded_files'):
+            os.remove(os.path.join('uploaded_files', f))
+    for uploaded_file in pdf_docs:
+        if not os.path.isdir('uploaded_files'):
+            os.mkdir('uploaded_files')
+        with open(os.path.join('uploaded_files', uploaded_file.name), 'wb') as f:
+            f.write(uploaded_file.getvalue())
+        st.success('File has been uploaded and saved to the session.')
+
+    # load the pdfs
+    loder = PyPDFDirectoryLoader("uploaded_files")
+    docs = loder.load()
+    st.write(f"Loaded {len(docs)} PDFs")
+    st.write(f'Type: {type(docs)}')
+    return docs
 
 
 def get_text_chunks(text):
-    text_splitter = CharacterTextSplitter(
-        separator="\n",
+    text_splitter = RecursiveCharacterTextSplitter(
         chunk_size=1000,
         chunk_overlap=200,
         length_function=len
     )
-    chunks = text_splitter.split_text(text)
+    chunks = text_splitter.split_documents(text)
     return chunks
 
 
@@ -46,7 +68,8 @@ def get_vectorstore(text_chunks):
         model_kwargs=model_kwargs,
         encode_kwargs=encode_kwargs
     )
-    vectorstore = FAISS.from_texts(texts=text_chunks, embedding=embeddings)
+    st.write(f"Loaded embeddings model: {text_chunks}")
+    vectorstore = FAISS.from_documents(text_chunks, embeddings)
     return vectorstore
 
 
@@ -74,15 +97,18 @@ def get_conversation_chain(vectorstore):
 
     conversation_chain = ConversationalRetrievalChain.from_llm(
         llm=llm,
-        retriever=vectorstore.as_retriever(),
-        memory=memory
+        retriever=vectorstore.as_retriever(search_kwargs={"k": 2}),
+        memory=memory,
+        chain_type="stuff",
+        condense_question_prompt=prompt,
     )
 
     return conversation_chain
 
 
 def handle_userinput(user_question):
-    response = st.session_state.conversation({'question': user_question})
+    response = st.session_state.conversation({'question': user_question,
+                                              'chat_history': st.session_state.chat_history})
     print(response)
     st.session_state.chat_history = response['chat_history']
 
